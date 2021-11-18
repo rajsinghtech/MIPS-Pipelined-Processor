87c46026893eb1d016d0ea7250f38e9e890919eb
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-11-18 16:33:55 -0600

    Re-arrange use newest value first

--- Testing_with_forwarding/src/forwarding_unit.vhd
+++ Testing_with_forwarding/src/forwarding_unit.vhd
@@ -20,13 +20,13 @@ architecture structural of forwarding_unit is
 
 begin
 
-    rs_select <= "01" when rs_addr = wb_mem_addr
-            else "10" when rs_addr = wb_wb_addr
+    rs_select <= "10" when rs_addr = wb_wb_addr
+            else "01" when rs_addr = wb_mem_addr
             else "11" when rs_addr = wb_ex_addr
             else "00";
 
-    rt_select <= "01" when rt_addr = wb_mem_addr
-            else "10" when rt_addr = wb_wb_addr
+    rt_select <= "10" when rt_addr = wb_wb_addr
+            else "01" when rt_addr = wb_mem_addr
             else "11" when rs_addr = wb_ex_addr
             else "00";
 
