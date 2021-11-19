418d36bfd6644d3bb1f57701db2097c170f000f9
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-11-18 18:07:35 -0600

    Update MIPS_Processor.vhd

--- Testing_with_forwarding/src/MIPS_Processor.vhd
+++ Testing_with_forwarding/src/MIPS_Processor.vhd
@@ -589,7 +589,7 @@ port map( i_S => control_sigs_EX(27),
   port map(
     i_CLK => iCLK,
     i_RST => iRST,
-    i_WE => not (control_sigs_EX(10) or control_sigs_EX(0) or take_branch),
+    i_WE => '1',
     i_D(31 downto 0) => wb_data_EX,        -- alu out
     i_D(63 downto 32) => rt_EX,            -- jump address
     i_D(68 downto 64) => final_wb_addr_EX, -- mem write data
