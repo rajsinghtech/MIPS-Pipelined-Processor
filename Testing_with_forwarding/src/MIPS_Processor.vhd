0d9aa3de6229b5f807036accdfe420034ecd612b
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-11-18 19:37:57 -0600

    Update MIPS_Processor.vhd

--- Testing_with_forwarding/src/MIPS_Processor.vhd
+++ Testing_with_forwarding/src/MIPS_Processor.vhd
@@ -117,6 +117,8 @@ end  MIPS_Processor;
 
   -- Memory Signals
 
+  signal clear_stage : std_logic;
+
   signal wb_data_MEM             : std_logic_vector(N-1 downto 0);
   signal wb_addr_MEM                  : std_logic_vector(4 downto 0);
 
@@ -299,6 +301,13 @@ end  MIPS_Processor;
       
       end component;
       
+      component dffg is
+        port(i_CLK        : in std_logic;     -- Clock input
+             i_RST        : in std_logic;     -- Reset input
+             i_WE         : in std_logic;     -- Write enable input
+             i_D          : in std_logic;     -- Data value input
+             o_Q          : out std_logic);   -- Data value output
+      end component;      
 
 begin
 
@@ -464,7 +473,7 @@ raw_ins_D <= fetch_stage_reg(31 downto 0);
     generic map(N => 191)
     port map(
       i_CLK => iCLK,
-      i_RST => iRST or (control_sigs_EX(10) and not iCLK ),
+      i_RST => iRST,
       i_WE => not stall,
       i_D(31 downto 0) => raw_ins_D,
       i_D(63 downto 32) => jal_return_D,
@@ -593,12 +602,19 @@ port map( i_S => control_sigs_EX(27),
 
 -- Execute state registers
 
+  delay_clear: dffg
+  port map( i_CLK => iCLK,
+            i_RST => iRST,
+            i_WE => '1',
+            i_D => control_sigs_EX(10),
+            o_Q => clear_stage);
+
 
   EX_MEM_Reg: dffg_N
   generic map(N => 73)
   port map(
     i_CLK => iCLK,
-    i_RST => iRST,
+    i_RST => iRST or clear_stage,
     i_WE => '1',
     i_D(31 downto 0) => wb_data_EX,        -- alu out
     i_D(63 downto 32) => rt_EX,            -- jump address
