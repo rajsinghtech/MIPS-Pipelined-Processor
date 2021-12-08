365da40d0774f6be6bd7ca3b3d136b9f97d7588a
Liz <elizbethlfransen@gmail.com>
2021-12-08 10:55:18 -0600

    remove spaces

--- Testing_with_forwarding/mips/Proj1_bubblesort.s
+++ Testing_with_forwarding/mips/Proj1_bubblesort.s
@@ -21,31 +21,22 @@ main:
     lui $s0, 0x1001
     # i($s2) = 0
     addi $s2, $zero, 0
-    ori $s1, $s1, 0x0028
-    nop
-    nop
-    lw $s1, 0($s1)
-    nop
-    
+    ori $s1, $s1, 0x0028    
+    lw $s1, 0($s1)    
     # outer loop
 outer_loop_cond:
-    nop
     addi $t0, $s1, -1
-    nop
-    nop
     # $t1 = i < n - 1
     slt $t1, $s2, $t0
-    nop
-    nop
     bne $t1, $zero, outer_loop_body
     j exit_outer_loop
 outer_loop_body:
-    nop
+    
     add $s4, $zero, $zero
     # j($s3) = 0
     add $s3, $zero, $zero
 inner_loop_cond:
-    nop
+    
     sub  $t0, $s1, $s2
     addi $t0, $t0, -1
     # $t0 = j < n - i - 1
@@ -53,7 +44,7 @@ inner_loop_cond:
     bne $t0, $zero, inner_loop_body
     j exit_inner_loop
 inner_loop_body:
-    nop
+    
     # get offset of j (j * 4)
     sll $t0, $s3, 2
     add $s5, $s0, $t0
@@ -71,19 +62,17 @@ inner_loop_body:
     sw $t1, 0($s5)
     addi $s4, $zero, 1
 inner_loop_footer:
-    nop
     addi $s3, $s3, 1
     j inner_loop_cond
 exit_inner_loop:
-    nop
+    
     beq $s4, $zero, exit_outer_loop
 outer_loop_footer:
     addi $s2, $s2, 1
     j outer_loop_cond
 exit_outer_loop:
-    nop
     # Exit program
     j exit
 exit:
-    nop
+    
     halt
